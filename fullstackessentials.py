af02fa1d4e52f377eb6fe0dce3a5339f5e3f8cd0
jonahbardos <jonahbardos@gmail.com>
2019-04-16 20:20:31 -0700

    2nd

--- fullstackessentials.py
+++ fullstackessentials.py
@@ -3,3 +3,8 @@ def PrintOneTwoFiftyFive():
     for x in range(0,256):
         print(x)
 
+# Write a program that would print all the odd numbers from 1 to 1000
+def PrintOdds():
+    for x in range(1,1001):
+        if x % 2 == 1:
+            print(x)
