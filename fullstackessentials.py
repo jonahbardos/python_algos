73c5248b7c774482a1632ef51d0de7cb3576fd91
jonahbardos <jonahbardos@gmail.com>
2019-04-16 20:22:47 -0700

    third

--- fullstackessentials.py
+++ fullstackessentials.py
@@ -8,3 +8,11 @@ def PrintOdds():
     for x in range(1,1001):
         if x % 2 == 1:
             print(x)
+
+# Write a program that would print the sum of all the odd numbers from 1 to 5000
+def SumOdds():
+    sum = 0
+    for x in range(1,5001):
+        if x % 2 == 0:
+            sum += x
+    print(sum)
